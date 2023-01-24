e79ab8dfa5fedd3ad348fa27bab88a48bcd4bb93
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2023-01-24 12:38:28 -0600

    add docker tag

--- commands.sh
+++ commands.sh
@@ -16,17 +16,17 @@ docker rm <container_id>
 #command to build sql server container
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$SA_PASSWORD" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2017-latest
 
-#run data-load container, since we are using bridge you will want to use the SQL_SERVER as the local IP of the machine.
-docker run --network bridge -e SQLFQDN=$SQL_SERVER -e SQLUSER=SA -e SQLPASS=$SA_PASSWORD -e SQLDB=mydrivingDB registryqzu2798.azurecr.io/dataload:1.0
-
 #command to build seperate images
 docker build -t <image_name> .
 
-#tag the images with the registry
-
+#run data-load container, since we are using bridge network you will want to use the SQL_SERVER as the local IP of the machine.
+docker run --network bridge -e SQLFQDN=$SQL_SERVER -e SQLUSER=SA -e SQLPASS=$SA_PASSWORD -e SQLDB=mydrivingDB registryqzu2798.azurecr.io/dataload:1.0
 
 #command to connect to registry
 az acr login --name <registry_name>
+
+#tag the images with the registry
+docker tag <source_image>:<tag> <registry_name>:<tag>
 
 #push image to registry_name
 docker push <registry_url>/<image_name>:<version>
